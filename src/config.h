251f6ce94eb1402e92d3339bbcd28b422507bbde
twofei <anhbk@qq.com>
2014-11-15 22:25:32 +0800

    add config for win32 source building

---
+++ src/config.h
@@ -0,0 +1,9 @@
+// Confituration file for libtccw32
+// By twofei <anhbk@qq.com> @ 2014-11-15
+
+#define TCC_VERSION "0.9.26"
+#define TCC_TARGET_PE
+#define TCC_TARGET_I386
+#define ONE_SOURCE
+
+#define _CRT_SECURE_NO_WARNINGS
